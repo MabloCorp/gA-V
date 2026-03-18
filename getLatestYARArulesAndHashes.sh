c233af870b37e3bdf1094b138fdcf11fff6e8f12
Antoine <69725365+Shynif@users.noreply.github.com>
2026-03-18 09:32:03 +0100

    Add fetching latest hashes from Bazaar in Bash script

--- getLatestYARArulesAndHashes.sh
+++ getLatestYARArulesAndHashes.sh
@@ -6,6 +6,12 @@ unzip yara-forge-rules-full.zip
 wget https://yaraify.abuse.ch/yarahub/yaraify-rules.zip
 unzip yaraify-rules.zip -d yaraify
 
+wget https://bazaar.abuse.ch/export/txt/sha256/full
+unzip full
+export SHA_FILE=full_sha256.txt
+tail -n +10 "$SHA_FILE" > "$SHA_FILE.tmp" && mv "$SHA_FILE.tmp" "$SHA_FILE"
+head -n -2  "$SHA_FILE" > "$SHA_FILE.tmp" && mv "$SHA_FILE.tmp" "$SHA_FILE"
 
 rm yara-forge-rules-full.zip
 rm yaraify-rules.zip
+rm full
